f2b1ac654b83fba5d50bce590813a302aded5053
Sai <s.sharan2008@gmail.com>
2017-08-28 00:34:03 -0400

    added <li> </li> and its  properties

---
+++ resource/js/main.js
@@ -0,0 +1,9 @@
+// user has clicked the button
+// if user adds text , add it to the list
+
+document.getElementById('add').addEventListener('click', function () {
+  var value =document.getElementById('item').value;
+  if (value) {
+
+  }
+});
